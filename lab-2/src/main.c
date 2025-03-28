beb930a1ee45f755d920adebbdc5d8dd2b4464a1
teotoivo <teo.maximilien@gmail.com>
2025-03-28 14:51:25 +0200

    w

--- lab-2/src/main.c
+++ lab-2/src/main.c
@@ -1,4 +1,5 @@
 #include "pico/stdlib.h"
+#include "pico/util/queue.h"
 #include "stdio.h"
 #include "utils.h"
 
@@ -11,35 +12,39 @@
 #define ROT_A 10
 #define ROT_B 11
 
-bool led_on = false;
-int brightness = 500;
-int scroll_speed = 100;
+#define DEFAULT_BRIGHTNESS 500
 
-void gpio_callback(uint gpio, uint32_t events) {
+#define SCROLL_SPEED 75
+
+static bool led_on = false;
+volatile static int brightness = DEFAULT_BRIGHTNESS;
+
+static queue_t events;
+
+void gpio_callback(uint gpio, uint32_t event_mask) {
   // events is checked with the & operator because it is a bit mask this will
   // check if one of the events is GPIO_IRQ_EDGE_RISE
-  if (gpio == ROT_A && (events & GPIO_IRQ_EDGE_RISE)) {
+  if (gpio == ROT_A && (event_mask & GPIO_IRQ_EDGE_RISE)) {
+
     if (gpio_get(ROT_B) == 0) {
-      // clockwise trun
+      // clockwise turn
       if (led_on) {
-        brightness += scroll_speed;
-        if (brightness > 1000) {
-          brightness = 1000;
-        }
+        int rot_direction = 1;
+        queue_try_add(&events, &rot_direction);
       }
     } else {
       // counter clockwise turn
       if (led_on) {
-        brightness -= scroll_speed;
-        if (brightness < 0) {
-          brightness = 0;
-        }
+        int rot_direction = -1;
+        queue_try_add(&events, &rot_direction);
       }
     }
   }
 }
 
 int main() {
+  stdio_init_all();
+
   bool sw_held_down = false;
 
   init_button_pin(ROT_SW);
@@ -50,27 +55,41 @@ int main() {
   setup_pwm(LED_D2);
   setup_pwm(LED_D3);
 
+  queue_init(&events, sizeof(int), 10);
+
   // sets the callback for all enabled interrupts
   gpio_set_irq_enabled_with_callback(ROT_A, GPIO_IRQ_EDGE_RISE, true,
                                      &gpio_callback);
   // Loop forever
   while (true) {
+    int rot_direction;
+    while (queue_try_remove(&events, &rot_direction)) {
+      brightness += rot_direction * SCROLL_SPEED;
+    }
+
     if (sw_held_down && !button_pressed(ROT_SW)) {
       // if button is released reset sw_held_down
       sw_held_down = false;
     } else if (!sw_held_down && button_pressed(ROT_SW)) {
       // if button is pressed for the fisrt time toggle led
-      led_on = !led_on;
 
       if (led_on && brightness == 0) {
         // if toggled on and brightnes is zero reset brightness to 500
-        brightness = 500;
+        brightness = DEFAULT_BRIGHTNESS;
+      } else {
+        led_on = !led_on;
       }
       sw_held_down = true;
-      sleep_ms(200); // wait to avoid switch bounce
+      sleep_ms(250); // wait to avoid switch bounce
     }
 
     if (led_on) {
+      printf("%d\n", brightness);
+      if (brightness > 1000)
+        brightness = 1000;
+      if (brightness < 0)
+        brightness = 0;
+
       printf("%d\n", brightness);
       set_brightness(LED_D1, brightness);
       set_brightness(LED_D2, brightness);
